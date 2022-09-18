12a2f686cfcc6149a4a7180130eaa90678766684
trungb19 <trungb1910322@gmail.com>
2022-09-18 20:38:56 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 //cors dùng để chia sẻ tài nguyên chéo, vd clien gọi API từ 1 nguồn khác có domain khác với trang hiện tại
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
+
 const app = express();
 
 app.use(cors());
@@ -12,5 +14,6 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application!"});
 });
 
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
 
